86ef4344260c9fa7dfcd60bcf95e4395a8543eb1
JoeSuber <joesuber@gmail.com>
2014-04-25 00:05:36 -0500

    headerstring

--- mingo.py
+++ mingo.py
@@ -1,6 +1,6 @@
 __author__ = 'suber1'
 """
-1) allow import of all csv-style sheets via user created templates
+1) allow import of all csv-style data via user created templates
 2) templates are stored in database along with the data and a record of imported file-names
 3) templates can be found by a header-string that matches against the incoming header
     if no match, user is prompted to provide the mapping of headers-to-db-collection-columns
@@ -76,7 +76,7 @@ class CsvMapped(dict):
     collection dicts.  Mappings can be defined here and also load, if available,
     pickled mappings created earlier during interactive matching process. Goal is
     to map a csv-source file to database once & from that time forward, have
-    similar documents automatically recognized, even if database is unavailable.
+    similar documents automatically recognized, even if old database is unavailable.
     """
     def __init__(self, atlas={}, mapfile='passed_in_csv_headers.pkl'):
         """
@@ -112,6 +112,56 @@ class CsvMapped(dict):
                     print(" but it isn't a pickle.  Moving on...")
         self.atlas = atlas
         self.pickle_fn = mapfile
+        self.thetext = ''
+
+    def fn_ctime(self, fn):
+        """
+        attach c_time os-provided info to filename in a consistent way
+        """
+        return unicode(fn + '|' + unicode(os.stat(fn).st_ctime))
+
+
+    def csvsources(self, usedcsvdb, startdir=None, looking_for='*.csv'):
+        """
+        gather and track the input data (saved in csv format) for
+         later importation into the mongodb. Return only valid choices as dict.
+        """
+        if not startdir:
+            startdir = os.path.expanduser('~')
+
+        # paths to CSV files,
+        csvfiles = [os.path.join(root, filename)
+                    for root, dirnames, filenames in os.walk(startdir)
+                    for filename in filenames if filename.endswith(looking_for)]
+        fn_dd = {ctr: fn for ctr, fn in enumerate(csvfiles)}
+
+        # create unique key for filename to prevent re-import of the same data
+        longfnkey = {}
+        for num, fnv in fn_dd.viewitems():
+            longfnkey.update({self.fn_ctime(fnv): num})
+
+        # check already used 'imported_flnms' database against glob filenames
+        alreadyused = []
+        examining = longfnkey.keys()
+        for checking in usedcsvdb.find():
+            # todo: don't forget to update database with filenames we use later!
+            if checking in examining:
+                deadnum = longfnkey[checking]
+                alreadyused.append(deadnum)
+                print('{:4} Already Imported: {}'.format(deadnum, fn_dd.pop(deadnum)))
+        print("There are {} files with extension {} we have already imported into database"
+              .format(len(alreadyused), looking_for))
+        # re-index choices into consecutive digits
+        # add in an option to not do anything
+        showfn_dd = {num: fnp for num, fnp in zip(xrange(len(fn_dd)), fn_dd.values())}
+        showfn_dd.update({len(fn_dd): ' - NONE - '})
+        return showfn_dd
+
+    def construct_header(self, fpath, online=0, spliton=','):
+        with open(fpath, 'rU') as fob:
+            self.thetext = fob.read().splitlines()
+        
+        return "|".join([h.strip() for h in self.thetext[online].split(spliton)])
 
     def headers_to_mongo(self, db):
         """
@@ -128,7 +178,7 @@ class CsvMapped(dict):
 
 
 if __name__ == "__main__":
-    looking_for = '*.csv'
+
     #dbserverip = '192.168.0.105'
     dbserverip = 'localhost'
     dbserverport = 27017
@@ -136,54 +186,42 @@ if __name__ == "__main__":
 
     # user chooses the database and collection we are messing with:
     overalldb, stuffdb = explore(0, MongoClient(dbserverip, dbserverport))
+
     # previously found mappings between CSV and DB columns:
     hdrs = overalldb['headers_map']
     importedfn = overalldb['imported_flnms']
 
-    # paths to CSV files,
-    csvfiles = [os.path.join(root, filename)
-                for root, dirnames, filenames in os.walk(os.path.expanduser('~'))
-                for filename in filenames if filename.endswith(looking_for)]
-    fn_dd = {ctr: fn for ctr, fn in enumerate(csvfiles)}
+    # find database collection labels for 'explored' stuffdb
+    labelset = set()
+    for hp in stuffdb.find():
+        labelset = set(kk[0] for kk in hp.viewitems())
+    print('labelset (from things already in db): {}'.format(labelset))
 
-    # create unique key for filename to prevent re-import of the same data
-    longfnkey = {}
-    for num, fnv in fn_dd.viewitems():
-        longfnkey.update({unicode(fnv + '|' + unicode(os.stat(fnv).st_ctime)): num})
+    # do mappings map to database?
 
-    # check already used 'imported_flnms' database against glob filenames
-    alreadyused = []
-    examining = longfnkey.keys()
-    for checking in importedfn.find():
-        # todo: don't forget to update database with filenames we use later!
-        if checking in examining:
-            deadnum = longfnkey[checking]
-            alreadyused.append(deadnum)
-            print('{:4} Already Imported: {}'.format(deadnum, fn_dd.pop(deadnum)))
-    print("There are {} files with extension {} we have already imported into database"
-          .format(len(alreadyused), looking_for))
-    # re-index choices into consecutive digits
-    # add in an option to not do anything
-    showfn_dd = {num: fnp for num, fnp in zip(xrange(len(fn_dd)), fn_dd.values())}
-    showfn_dd.update({len(fn_dd): ' - NONE - '})
+    # validate found source files for potential input into db
+    new_fn_dd = xmarks.csvsources(importedfn, startdir=None, looking_for='*.csv')
 
-    # now have user choose one:
-    YOUMAYPASS = False
-    while not YOUMAYPASS:
-        selnum, fpath = selections(fn_dd, prompt='Above are saved CSV files you can add to mongod. Choose wisely: ')
-        if selnum in alreadyused:
-            YOUMAYPASS = False
-            print("that one has already been imported. Try again")
-        else:
-            YOUMAYPASS = True
-            print(" You chose wisely: {}".format(fpath))
+    # now have user choose one until none left or done:
+    selnum = 0
+    while selnum != (len(new_fn_dd) - 1):
+        selnum, fpath = selections(new_fn_dd, prompt='Above are saved CSV files you can add to mongod. Choose wisely: ')
+        if selnum != (len(new_fn_dd) - 1):
+            # open file, determine header
+            hdrs, np = '', 0
+            while not hdrs:
+                hdrs = xmarks.construct_header(fpath, online=np)
+                np += 1
+                if np > 60:
+
+            # look up header in database to find import map
+            # if no import-map, create the import-map
+                #including some rules about adding, subtracting quantities
+            # using the import-map, import the data
+            # on success, add the input filename to database of imported_fn, re-run csv-sources
 
     #
-    print('opening: {}'.format(fpath))
-    with open(fpath, 'rU') as fob:
-        thetext = fob.read().splitlines()
-    headers = thetext[0].split(',')
-    header_quant = len(headers)
+
     print('There are {} column-headers in {}: '.format(header_quant, fpath))
     print(" |".join(headers))
     print(" so... ")
@@ -206,11 +244,7 @@ if __name__ == "__main__":
             csvdocs[numer] = {h: cell.strip() for h, cell in zip(headers, lineparts)}
     print('{} items are to be gleaned from this CSV file: {}'.format(numer, fpath))
 
-    # find current database categories
-    labelset = set()
-    for hp in stuffdb.find():
-        labelset = set(kk[0] for kk in hp.viewitems())
-    print('labelset (from things already in db): {}'.format(labelset))
+
 
     # check if a mapping already has been made fitting some CSV-derived columns
     # (skipping above)
